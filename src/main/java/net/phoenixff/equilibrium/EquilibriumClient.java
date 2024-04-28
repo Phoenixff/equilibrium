fa7136e6224256684a1fb8a2d7e1200fe0e727a8
Phoenixff <felix-f99@web.de>
2024-04-28 22:51:17 +0200

    Initial commit

---
+++ src/main/java/net/phoenixff/equilibrium/EquilibriumClient.java
@@ -0,0 +1,10 @@
+package net.phoenixff.equilibrium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EquilibriumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/phoenixff/equilibrium/EquilibriumDataGenerator.java
@@ -0,0 +1,11 @@
+package net.phoenixff.equilibrium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EquilibriumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
